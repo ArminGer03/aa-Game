7ed349fd1b2a9cfc7ed51f6121450b1a0481713c
ArminGer03 <a1382.geramirad@gmail.com>
2023-05-23 15:22:03 +0330

    Made MainMenu.fxml

--- aa-Game/src/main/java/view/Controllers/MainMenuGController.java
+++ aa-Game/src/main/java/view/Controllers/MainMenuGController.java
@@ -1,4 +1,20 @@
 package view.Controllers;
 
+import javafx.scene.input.MouseEvent;
+
 public class MainMenuGController {
+    public void mainMenu(MouseEvent mouseEvent) {
+    }
+
+    public void profileMenu(MouseEvent mouseEvent) {
+    }
+
+    public void settingsMenu(MouseEvent mouseEvent) {
+    }
+
+    public void scoreBoard(MouseEvent mouseEvent) {
+    }
+
+    public void exit(MouseEvent mouseEvent) {
+    }
 }
