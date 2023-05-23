f5bbbfded9e27c30fcb2b91f4c613dab184e470a
ArminGer03 <a1382.geramirad@gmail.com>
2023-05-23 17:27:37 +0330

    ProfileMenu updated

--- aa-Game/src/main/java/view/Controllers/MainMenuGController.java
+++ aa-Game/src/main/java/view/Controllers/MainMenuGController.java
@@ -1,12 +1,35 @@
 package view.Controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import view.MainMenu;
+import view.SignUpMenu;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class MainMenuGController {
+
+    @FXML
+    private Label mainLabel;
+    @FXML
+    private BorderPane borderPane;
+    @FXML
+    private AnchorPane anchorPane;
+
     public void mainMenu(MouseEvent mouseEvent) {
+        borderPane.setCenter(anchorPane);
     }
 
-    public void profileMenu(MouseEvent mouseEvent) {
+    public void profileMenu(MouseEvent mouseEvent) throws IOException {
+        URL url = MainMenu.class.getResource("/fxml/ProfileMenu.fxml");
+        Parent root = FXMLLoader.load(url);
+        borderPane.setCenter(root);
     }
 
     public void settingsMenu(MouseEvent mouseEvent) {
@@ -16,5 +39,14 @@ public class MainMenuGController {
     }
 
     public void exit(MouseEvent mouseEvent) {
+    }
+
+    public void startGame(MouseEvent mouseEvent) {
+    }
+
+    public void resumeGame(MouseEvent mouseEvent) {
+    }
+
+    public void start1v1Game(MouseEvent mouseEvent) {
     }
 }
--- aa-Game/src/main/java/view/MainMenu.java
+++ aa-Game/src/main/java/view/MainMenu.java
@@ -1,5 +1,26 @@
 package view;
 
-public class MainMenu {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.*;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import utility.DataClass;
 
+import java.net.URL;
+
+public class MainMenu extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        DataClass.setStage(stage);
+        URL url = LoginMenu.class.getResource("/fxml/MainMenu.fxml");
+        BorderPane borderPane = FXMLLoader.load(url);
+        Scene scene = new Scene(borderPane,650,400);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
